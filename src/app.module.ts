0e9f64e9f5516fcbf673df17f74f41c0f5ca1060
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-06-18 23:52:53 -0300

    feat: docker mysql database and port to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigModule, UserModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT ?? 4444;
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4444);
+  await app.listen(PORT);
 }
 bootstrap();
